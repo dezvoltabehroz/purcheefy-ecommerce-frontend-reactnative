77d81ee9585f09711d137dec9c9a182c11573186
bb-grey <bilal.ahmad@shopdev.co>
2024-02-24 19:27:19 +0500

    Added Splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
+import {useEffect} from 'react';
 import RootNavigation from 'navigation/RootNavigation';
+import BootSplash from 'react-native-bootsplash';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import defaultStyles from 'utils/defaultStyles';
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
+
   return (
     <GestureHandlerRootView style={defaultStyles.flex1}>
       <RootNavigation />
